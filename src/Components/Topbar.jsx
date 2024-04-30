d4e2b5e1dfb83b1114bdfc0a7458ac81a328ca2a
Aman kumar <amank0259@gmail.com>
2024-04-30 18:16:01 +0530

    change UI

--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -6,8 +6,8 @@ import { MdMail } from "react-icons/md";
 function Topbar() {
     return (
         <div className='hidden bg-zinc-900 px-10 md:px-64 h-10 text-white md:flex items-center justify-between gap-10 absolute w-full top-0 left-0'>
-            <p><a href="tel:+917417013033"><MdPhoneInTalk className='inline-block' /> 7417013033</a></p>
-            <p><a href="mailto:Info@paradiseuttarakhand.com"><MdMail className='inline-block' /> Info@paradiseuttarakhand.com</a></p>
+            <p className='hover:underline decoration-wavy'><a href="tel:+917417013033"><MdPhoneInTalk className='inline-block' /> 7417013033</a></p>
+            <p className='hover:underline decoration-wavy'><a href="mailto:Info@paradiseuttarakhand.com"><MdMail className='inline-block' /> Info@paradiseuttarakhand.com</a></p>
         </div>
     )
 }
