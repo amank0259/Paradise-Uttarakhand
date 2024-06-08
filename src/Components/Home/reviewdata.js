1832ba9d86a5eec439ba10298e24b473e044e4f8
Aman kumar <amank0259@gmail.com>
2024-06-08 17:49:40 +0530

    add customer feedback and review component

---
+++ src/Components/Home/reviewdata.js
@@ -0,0 +1,46 @@
+const reviewData = [
+    {
+        id: 0,
+        name: 'Manik',
+        review: 'Discovering Paradise Uttarakhand was like finding a hidden gem in nature embrace. The lush greenery, snow-capped peaks, and pristine rivers create a surreal backdrop for an unforgettable journey. The accommodations seamlessly blend luxury with nature, providing an unparalleled experience.',
+        rate: 4,
+        img: ''
+    },
+    {
+        id: 1,
+        name: 'Manav Bansal',
+        review: 'Paradise Uttarakhand truly lives up to its name! Nestled in the lap of the Himalayas, this destination offers a breathtaking escape. The serene landscapes, charming local culture, and impeccable hospitality make it an absolute paradise for any traveler seeking tranquility.',
+        rate: 5,
+        img: ''
+    },
+    {
+        id: 2,
+        name: 'Shivam Rai',
+        review: 'Paradise Uttarakhand is a haven for adventure enthusiasts. Whether the trekking in the Himalayas, river rafting in the Ganges, or exploring dense forests, this destination offers an adrenaline-packed experience. The well-organized excursions and knowledgeable guides make every adventure safe and exhilarating.',
+        rate: 3.5,
+        img: ''
+    },
+    {
+        id: 3,
+        name: 'Kartavya',
+        review: 'Immerse yourself in the rich cultural tapestry of Uttarakhand at Paradise. The local traditions, vibrant festivals, and warm hospitality of the people make it a unique and authentic experience. Every corner tells a story, creating memories that will last a lifetime.',
+        rate: 5,
+        img: ''
+    },
+    {
+        id: 4,
+        name: 'Sawan',
+        review: 'Paradise Uttarakhand is the perfect destination for a family getaway. The variety of activities, from nature walks to cultural experiences, cater to all age groups. The warm and welcoming atmosphere ensures a memorable vacation for the whole family.',
+        rate: 5,
+        img: ''
+    },
+    {
+        id: 5,
+        name: 'Piyush',
+        review: 'The staff at Paradise Uttarakhand goes above and beyond to make your stay exceptional. Their attention to detail, prompt service, and friendly demeanor create a welcoming atmosphere. From the moment you arrive until your departure, you are treated like royalty.',
+        rate: 4,
+        img: ''
+    },
+];
+
+export default reviewData;
