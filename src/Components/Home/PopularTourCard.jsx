06dc9d4ad3a3a0c878d7852056a7ebb432598dd4
Aman kumar <amank0259@gmail.com>
2024-06-10 08:58:07 +0530

    minor changes

--- src/Components/Home/PopularTourCard.jsx
+++ src/Components/Home/PopularTourCard.jsx
@@ -8,11 +8,11 @@ function PopularTourCard({ data }) {
 
     return (
         <div className='w-full'>
-            <h1 className='text-center py-4 font-semibold md:text-2xl'>Explore By Fixed Departures
-            </h1>
+            <h3 className='text-center py-4 font-semibold md:text-2xl'>Explore By Fixed Departures
+            </h3>
             <div className='h-full flex flex-wrap md:gap-6 justify-between'>
                 {data.map((item, index) => (
-                    <div key={index} onClick={() => navigate(`Tour/${item.link}`)} className='h-[45vh] sm:w-[100%] md:w-[48%] lg:w-[30%] rounded-xl cursor-pointer duration-300 shadow-md hover:shadow-md hover:shadow-yellow-500'>
+                    <div key={index} onClick={() => navigate(`Tour/${item.link}`)} className='h-[45vh] sm:w-[100%] md:w-[48%] lg:w-[30%] rounded-xl cursor-pointer duration-300 shadow-md hover:shadow-md border hover:shadow-yellow-500'>
                         <img className='w-[430px] h-[310px] rounded-t-lg' src={item.Img} alt="" />
                         <h3 className='font-semibold px-4 pt-2'>{item.name}</h3>
                         <p className='w-[90%] text-justify px-4 flex items-center gap-2'><MdDateRange /> {item.details}</p>
